7d4797e085880d0c151774473dec84280dae7f05
Kenneth Carter <kxccarter@gmail.com>
2020-07-01 16:33:09 -0500

    created new route for albums in server.js.

--- server/server.js
+++ server/server.js
@@ -1,14 +1,10 @@
 const express = require('express');
 const songList = require('./public/scripts/song');
 const artistList = require('./public/scripts/artist');
-
+const albumList = requite('./public/scripts/albums');
 
 const app = express();
 const PORT = 5000;
-
-
-
-
 
 app.use(express.static('server/public'));
 // the above code tells where requests for static files should look.
@@ -23,6 +19,10 @@ app.get('/artist', (req, res) => {
 app.get('/songs', (req, res) => {
     res.send(songList);
 });
+
+app.get('/album', (req, res) => {
+    res.send(artistList)
+})
 
 app.listen(PORT, () => {
     console.log('listening on port', PORT)

5ea234aeb2208628e94e06684a01b11dc22dfb0e
Kenneth Carter <kxccarter@gmail.com>
2020-07-01 14:51:09 -0500

    moved song and artist array to external modules and then imported them into server.js.

--- server/server.js
+++ server/server.js
@@ -1,60 +1,27 @@
 const express = require('express');
+const songList = require('./public/scripts/song');
+const artistList = require('./public/scripts/artist');
+
 
 const app = express();
 const PORT = 5000;
 
-const artistListArray = [{
-        name: 'Miles Davis',
-        born: 1926,
-        died: 1990,
-    },
-    {
-        name: 'Duke Ellington',
-        born: 1899,
-        died: 1974,
-    },
-    {
-        name: 'John Coltrane',
-        born: 1926,
-        died: 1987,
-    },
-    {
-        name: 'Louis Daniel Armstrong',
-        born: 1901,
-        died: 1971,
-    },
-];
 
-const songListArray = [{
-        title: 'Take Five',
-        artist: 'The Dave Brubeck Quartet',
-    },
-    {
-        title: 'So What',
-        artist: 'Miles Davis',
-    },
-    {
-        title: 'Sing Sing Sing',
-        artist: 'Benny Goodman',
-    },
-    {
-        title: 'Take the "A" Train',
-        artist: 'The Dave Brubeck Quartet',
-    },
-];
+
+
 
 app.use(express.static('server/public'));
 // the above code tells where requests for static files should look.
 
 app.get('/artist', (req, res) => {
-    res.send(artistListArray);
+    res.send(artistList);
     // When /artist is requested, app.get sends artistListArray back as a response.
 });
 
 // TODO - Add GET for songs
 
 app.get('/songs', (req, res) => {
-    res.send(songListArray);
+    res.send(songList);
 });
 
 app.listen(PORT, () => {

194d00b983a39ccc89831a69a4a44502d6ca92d5
Kenneth Carter <kxccarter@gmail.com>
2020-07-01 16:29:24 -0500

    created albums.js

---
+++ server/public/scripts/albums.js
@@ -0,0 +1,19 @@
+const albumsArray = [{
+        title: 'Time Out',
+        year: 1959,
+    },
+    {
+        title: 'Kind of Blue',
+        year: 1959,
+    },
+    {
+        title: 'Sing Sing Sing',
+        year: 1937,
+    },
+    {
+        title: 'Dave Brubek in Berlin',
+        year: 1965,
+    },
+];
+
+module.exports = albumsArray;
